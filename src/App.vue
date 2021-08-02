7c97cfec83d53ae103081f554e170335b5909f7c
kellykelly0808 <kellykelly0808@bitbucket.org>
2021-08-02 17:24:21 +0800

    add env

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,19 @@
     | <router-link to="/new">New</router-link>
   </div>
   <router-view/>
+   {{ name }}
+  <button type="button" class="btn btn-primary">Primary</button>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: process.env.VUE_APP_NAME
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import "bootstrap";
