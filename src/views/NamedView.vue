e3c6aab2eb5eb3e8251df22a6f8854009ac7cb74
kellykelly0808 <kellykelly0808@bitbucket.org>
2021-08-05 18:29:50 +0800

    add dynamicRouter

--- src/views/NamedView.vue
+++ src/views/NamedView.vue
@@ -1,15 +1,13 @@
 <template>
 <div class="container">
-    <h2>命名視圖</h2>
-    <div class="row">
-        <div class="col-6">
-        <router-view name="left"></router-view>
-        </div>
-        <div class="col-6">
-        <router-view name="right"></router-view>
-        </div>
-
-    </div>
+<h2>命名視圖</h2>
+<div class="row">
+<div class="col-6">
+<router-view name="left"></router-view>
 </div>
- 
+<div class="col-6">
+<router-view name="right"></router-view>
+</div>
+</div>
+</div>
 </template>
